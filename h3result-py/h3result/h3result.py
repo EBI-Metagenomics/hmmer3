081728975f89878b161ae53176127b0cfba0775b
Danilo Horta <danilo.horta@pm.me>
2023-06-19 10:52:00 +0100

    Refactor H3Result class methods to use updated function names in h3result library

--- h3result-py/h3result/h3result.py
+++ h3result-py/h3result/h3result.py
@@ -12,7 +12,7 @@ class H3Result:
     def __init__(self, filename: PathLike):
         self._cdata = ffi.NULL
 
-        self._cdata = lib.h3r_new()
+        self._cdata = lib.h3result_new()
         if self._cdata == ffi.NULL:
             raise MemoryError()
 
@@ -22,7 +22,7 @@ class H3Result:
         if fp == ffi.NULL:
             raise RuntimeError()
 
-        rc = lib.h3r_unpack(self._cdata, fp)
+        rc = lib.h3result_unpack(self._cdata, fp)
         lib.fclose(fp)
         if rc != 0:
             raise H3ResultError(rc)
@@ -30,27 +30,27 @@ class H3Result:
     def print_targets(self, fileno: int):
         fd = os.dup(fileno)
         fp = lib.fdopen(fd, b"w")
-        lib.h3r_print_targets(self._cdata, fp)
+        lib.h3result_print_targets(self._cdata, fp)
         lib.fclose(fp)
 
     def print_domains(self, fileno: int):
         fd = os.dup(fileno)
         fp = lib.fdopen(fd, b"w")
-        lib.h3r_print_domains(self._cdata, fp)
+        lib.h3result_print_domains(self._cdata, fp)
         lib.fclose(fp)
 
     def print_targets_table(self, fileno: int):
         fd = os.dup(fileno)
         fp = lib.fdopen(fd, b"w")
-        lib.h3r_print_targets_table(self._cdata, fp)
+        lib.h3result_print_targets_table(self._cdata, fp)
         lib.fclose(fp)
 
     def print_domains_table(self, fileno: int):
         fd = os.dup(fileno)
         fp = lib.fdopen(fd, b"w")
-        lib.h3r_print_domains_table(self._cdata, fp)
+        lib.h3result_print_domains_table(self._cdata, fp)
         lib.fclose(fp)
 
     def __del__(self):
         if getattr(self, "_cdata", ffi.NULL) != ffi.NULL:
-            lib.h3r_del(self._cdata)
+            lib.h3result_del(self._cdata)

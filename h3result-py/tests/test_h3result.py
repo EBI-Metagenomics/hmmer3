26f83d4b75b5747a763ac0c80abfe7e7635adbe9
Danilo Horta <danilo.horta@pm.me>
2023-06-19 10:51:12 +0100

    Add tests

---
+++ h3result-py/tests/test_h3result.py
@@ -0,0 +1,75 @@
+import os
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+
+import pytest
+
+from h3result.h3result import H3Result
+
+
+@pytest.fixture()
+def files_path() -> Path:
+    current_dir = os.path.dirname(__file__)
+    return Path(current_dir) / "files"
+
+
+class TempFile:
+    def __init__(self):
+        self._content = None
+        self._stream = None
+
+    def fileno(self):
+        return self._stream.fileno()
+
+    @property
+    def content(self):
+        assert isinstance(self._content, str)
+        return self._content
+
+    def __enter__(self):
+        self._stream = NamedTemporaryFile(delete=False)
+
+    def __exit__(self, *args, **kwargs):
+        self._stream.close()
+        self._content = open(self._stream.name).read()
+        os.unlink(self._stream.name)
+
+
+def test_targets(files_path: Path):
+    tmp = TempFile()
+    with tmp:
+        x = H3Result(files_path / "h3result.mp")
+        x.print_targets(tmp.fileno())
+
+    desired = open(files_path / "targets.txt").read()
+    assert desired == tmp.content
+
+
+def test_targets_table(files_path: Path):
+    tmp = TempFile()
+    with tmp:
+        x = H3Result(files_path / "h3result.mp")
+        x.print_targets_table(tmp.fileno())
+
+    desired = open(files_path / "targets_table.txt").read()
+    assert desired == tmp.content
+
+
+def test_domains(files_path: Path):
+    tmp = TempFile()
+    with tmp:
+        x = H3Result(files_path / "h3result.mp")
+        x.print_domains(tmp.fileno())
+
+    desired = open(files_path / "domains.txt").read()
+    assert desired == tmp.content
+
+
+def test_domains_table(files_path: Path):
+    tmp = TempFile()
+    with tmp:
+        x = H3Result(files_path / "h3result.mp")
+        x.print_domains_table(tmp.fileno())
+
+    desired = open(files_path / "domains_table.txt").read()
+    assert desired == tmp.content

1212a7e230b4a01b823ccb7e65850ab9da89cb43
Danilo Horta <danilo.horta@pm.me>
2024-08-11 02:26:46 +0100

    Add reentrant strerror and custom memccpy functions
    - Include errno.h for error handling
    - Implement strerror_reentrant for thread-safe error messages
    - Add memccpy_musl to replace standard memccpy
    - Update hmr_error_io to use strerror_reentrant
    - Modify header function to use memccpy_musl

--- hmmer-reader/error.c
+++ hmmer-reader/error.c
@@ -1,6 +1,7 @@
 #include "error.h"
 #include "hmr.h"
 #include <assert.h>
+#include <errno.h>
 #include <string.h>
 
 static char prefix[][20] = {
@@ -14,9 +15,26 @@ int hmr_error(int rc, char *dst, char const *msg)
     return rc;
 }
 
+static int strerror_reentrant(int err, char *buf, size_t buflen)
+{
+    char *msg = strerror(err);
+    size_t l = strlen(msg);
+    if (l >= buflen)
+    {
+        if (buflen)
+        {
+            memcpy(buf, msg, buflen - 1);
+            buf[buflen - 1] = 0;
+        }
+        return ERANGE;
+    }
+    memcpy(buf, msg, l + 1);
+    return 0;
+}
+
 int hmr_error_io(char *dst, int errnum)
 {
-    int rc = strerror_r(errnum, dst, HMR_ERROR_SIZE);
+    int rc = strerror_reentrant(errnum, dst, HMR_ERROR_SIZE);
     assert(!rc);
     (void)(rc);
     return HMR_EIO;
--- hmmer-reader/fsm.c
+++ hmmer-reader/fsm.c
@@ -232,6 +232,49 @@ static int arrow(struct args *a)
     return HMR_OK;
 }
 
+static void *memccpy_musl(void *restrict dest, const void *restrict src, int c,
+                          size_t n)
+{
+#define ALIGN (sizeof(size_t) - 1)
+#define ONES ((size_t) - 1 / UCHAR_MAX)
+#define HIGHS (ONES * (UCHAR_MAX / 2 + 1))
+#define HASZERO(x) ((x) - ONES & ~(x) & HIGHS)
+
+    unsigned char *d = dest;
+    const unsigned char *s = src;
+
+    c = (unsigned char)c;
+#ifdef __GNUC__
+    typedef size_t __attribute__((__may_alias__)) word;
+    word *wd;
+    const word *ws;
+    if (((uintptr_t)s & ALIGN) == ((uintptr_t)d & ALIGN))
+    {
+        for (; ((uintptr_t)s & ALIGN) && n && (*d = *s) != c; n--, s++, d++)
+            ;
+        if ((uintptr_t)s & ALIGN) goto tail;
+        size_t k = ONES * c;
+        wd = (void *)d;
+        ws = (const void *)s;
+        for (; n >= sizeof(size_t) && !HASZERO(*ws ^ k);
+             n -= sizeof(size_t), ws++, wd++)
+            *wd = *ws;
+        d = (void *)wd;
+        s = (const void *)ws;
+    }
+#endif
+    for (; n && (*d = *s) != c; n--, s++, d++)
+        ;
+tail:
+    if (n) return d + 1;
+    return 0;
+
+#undef HASZERO
+#undef HIGHS
+#undef ONES
+#undef ALIGN
+}
+
 static int header(struct args *a)
 {
     assert(a->tok->id == HMR_TOK_WORD || a->tok->id == HMR_TOK_NL);
@@ -249,8 +292,8 @@ static int header(struct args *a)
             a->pos->prof.end = a->pos->prof.begin + HMR_HEADER_MAX;
         }
         a->pos->prof.pos =
-            memccpy(a->pos->prof.pos - 1, a->tok->value, '\0',
-                    (unsigned long)(a->pos->prof.end - a->pos->prof.pos));
+            memccpy_musl(a->pos->prof.pos - 1, a->tok->value, '\0',
+                         (unsigned long)(a->pos->prof.end - a->pos->prof.pos));
     }
     else
     {

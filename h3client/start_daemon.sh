d26335f0d3ffdb4fa31f2af204f9900befe5790b
Danilo Horta <danilo.horta@pm.me>
2025-02-18 22:10:06 +0000

    Enhance start_daemon.sh with CLI install option
    - Modified start_daemon.sh to use `h3daemon[cli]` during installation
    - Ensured installation includes necessary CLI tools for optimal use

--- h3client/start_daemon.sh
+++ h3client/start_daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-command -v h3daemon >/dev/null || pipx install h3daemon
+command -v h3daemon >/dev/null || pipx install h3daemon[cli]
 
 h3daemon start data/ross.5.hmm --port 51371 >/dev/null
 h3daemon ready data/ross.5.hmm --wait       >/dev/null
